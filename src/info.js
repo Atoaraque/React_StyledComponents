408cb6bf1338788fbfb8345aa3bdcbd91eab4537
Alejandro Araque <j.alejandro.araque@gmail.com>
2023-09-18 14:43:50 -0500

    first commit

---
+++ src/info.js
@@ -0,0 +1,39 @@
+export const lista = {
+  cargos: [
+    {
+      id: "123131321",
+      type: "Restaurante",
+      value: "$56.000",
+      date: "11 JUN",
+      from: "Pizza Hut",
+    },
+    {
+      id: "4564654",
+      type: "Utilidades",
+      value: "$47.500",
+      date: "9 JUN",
+      from: "Cuenta de luz",
+    },
+    {
+      id: "65445",
+      type: "Salud",
+      value: "$27.750",
+      date: "8 JUN",
+      from: "Farmacias A.",
+    },
+    {
+      id: "656565",
+      type: "Transporte",
+      value: "$21.350",
+      date: "8 JUN",
+      from: "Uber",
+    },
+    {
+      id: "926544",
+      type: "Otros",
+      value: "$154.250",
+      date: "5 JUN",
+      from: "Amazon",
+    },
+  ],
+};
